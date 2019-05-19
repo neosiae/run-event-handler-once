1b4f1c68b4e4f9834a1272e3b8ad38b0cd7f3413
Denis Kotlica <neosiae@protonmail.com>
2019-05-19 19:43:07 +0200

    Run prettier on all JS files

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-module.exports = runEventHandlerOnce;
+module.exports = runEventHandlerOnce
 
 function runEventHandlerOnce(target, type, listener) {
   if (typeof listener !== 'function') {
-    throw new TypeError('The callback provided is not a function.');
+    throw new TypeError('The callback provided is not a function.')
   }
 
-  const options = arguments[3] || {};
-  const addOptions = options.add || {};
-  const removeOptions = options.remove || {};
+  const options = arguments[3] || {}
+  const addOptions = options.add || {}
+  const removeOptions = options.remove || {}
 
   function handleEvent(event) {
-    event.target.removeEventListener(event.type, handleEvent, removeOptions);
-    listener.apply(this, arguments);
+    event.target.removeEventListener(event.type, handleEvent, removeOptions)
+    listener.apply(this, arguments)
   }
 
-  target.addEventListener(type, handleEvent, addOptions);
+  target.addEventListener(type, handleEvent, addOptions)
 }
--- index.spec.js
+++ index.spec.js
@@ -1,39 +1,39 @@
-const test = require('tape');
-const sinon = require('sinon');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const runEventHandlerOnce = require('./index.js');
+const test = require('tape')
+const sinon = require('sinon')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+const runEventHandlerOnce = require('./index.js')
 
 test('runs an event handler', t => {
-  const dom = new JSDOM(`<!DOCTYPE html><button>Open</button>`);
-  const window = dom.window;
-  const button = window.document.querySelector('button');
-  const clickEvent = new window.MouseEvent('click');
-  const eventHandler = sinon.fake();
+  const dom = new JSDOM(`<!DOCTYPE html><button>Open</button>`)
+  const window = dom.window
+  const button = window.document.querySelector('button')
+  const clickEvent = new window.MouseEvent('click')
+  const eventHandler = sinon.fake()
 
-  runEventHandlerOnce(button, 'click', eventHandler);
-  button.dispatchEvent(clickEvent);
-  t.ok(eventHandler.called);
+  runEventHandlerOnce(button, 'click', eventHandler)
+  button.dispatchEvent(clickEvent)
+  t.ok(eventHandler.called)
 
-  window.close();
-  t.end();
-});
+  window.close()
+  t.end()
+})
 
 test('runs an event handler only once', t => {
-  const dom = new JSDOM(`<!DOCTYPE html><button>Open</button>`);
-  const window = dom.window;
-  const button = window.document.querySelector('button');
-  const clickEvent = new window.MouseEvent('click');
-  const eventHandler = sinon.fake();
+  const dom = new JSDOM(`<!DOCTYPE html><button>Open</button>`)
+  const window = dom.window
+  const button = window.document.querySelector('button')
+  const clickEvent = new window.MouseEvent('click')
+  const eventHandler = sinon.fake()
 
-  runEventHandlerOnce(button, 'click', eventHandler);
+  runEventHandlerOnce(button, 'click', eventHandler)
 
   for (let i = 0; i < 5; i++) {
-    button.dispatchEvent(clickEvent);
+    button.dispatchEvent(clickEvent)
   }
 
-  t.ok(eventHandler.calledOnce);
+  t.ok(eventHandler.calledOnce)
 
-  window.close();
-  t.end();
-});
+  window.close()
+  t.end()
+})
